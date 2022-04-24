9aa1fc1a6002fa23c68fddf131fc97bb3a03f2e1
sefammirza <sefammirza@gmail.com>
2022-04-24 20:37:18 +0200

    DateRingerPicker

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      {/* header */}
+      {/* banner */}
+      {/* cards */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
