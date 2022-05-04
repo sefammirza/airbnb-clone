a2387d58eb0a97ef4ba0482eaa5fd896c4f9815d
sefammirza <sefammirza@gmail.com>
2022-05-04 20:28:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SearchPage from './SearchPage';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header/>
-      <Home/>
-      {/* header */}
-      {/* banner */}
-      {/* cards */}
-      {/* footer */}
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/search" element={<SearchPage/>} />
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
+      
+      
+      
+      
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
 
-function Card({src, title, description, price}) {
+function Card({ src, title, description, price }) {
   return (
-    <div className='card'>
-        <img src={src} alt=""/>
-        <div className='card_info'>
-            <h2>{title}</h2>
-            <h4>{description}</h4>
-            <h3>{price}</h3>
-        </div>
+    <div className="card">
+      <img src={src} alt="" />
+      <div className="card_info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+function Card2({ src, title, description, price }) {
+  return (
+    <div className="card2">
+      <img src={src} alt="" />
+      <div className="card2_info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
+}
+
+function Card3({ src, title, description, price }) {
+  return (
+    <div className="card3">
+      <img src={src} alt="" />
+      <div className="card3_info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
+}
+export { Card, Card2, Card3 };
